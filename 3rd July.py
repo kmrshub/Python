88a6ca2d72f387b0c3a45fad364423b5be4bd62e
kmrshub <shubhamkmr1014@gmail.com>
2025-07-03 13:33:06 +0530

    Topics for 3rd July

---
+++ 3rd July.py
@@ -0,0 +1,22 @@
+# Print(Output) Function
+print ("Hello World")
+
+# Input Function
+name = input("Enter your name: ")
+print ("Hello,", name, "! Welcome!")
+
+val = input ("Enter your value : ")
+print (val) #By default the type of the returned object always will be String
+
+name = input ("What is your name?\n")
+print(name)
+
+#Typecasting input into Integer
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+print("Sum is: ", num1 + num2)
+
+#Typecasting input into Float
+num3 = float(input("Enter First Number: "))
+num4 = float(input("Enter Second Number: "))
+print("Sum is: ", num3 + num4)
