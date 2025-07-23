017fcf5f0c2d5d1d2816773c86cfdca716e36c96
kmrshub <shubhamkmr1014@gmail.com>
2025-07-23 12:42:23 +0530

    Added Python Keyword Topic

---
+++ Keywords.py
@@ -0,0 +1,28 @@
+import keyword
+print(keyword.kwlist) # This will print all the Keyword that is available in Python current version
+
+'''
+Categories of Keyword
+1. Control Flow Keyword
+   a. if, elif, else
+   b. while, for, break, continue, pass
+
+2. Function & Class Keyword
+   a. def, return, yield
+   b. lambda
+   c. class
+   
+3. Exception Handling - try, except, finally, raise, assert
+
+4. Variable Scope - global, nonlocal, del
+
+5. Boolean & NULL - True, False, None
+
+6. Logical Operator - and, or , not, is, in
+ 
+7. Importing Modulus - import, from, as
+
+8. Asynchronous Programming - async, await
+
+9. Pattern Matching - match, case
+'''
